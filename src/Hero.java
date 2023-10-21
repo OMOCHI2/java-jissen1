941cd270ae8607477471390f486539eb9e7f42b4
OMOCHI <satsukisama.mochi@gmail.com>
2023-10-22 00:02:40 +0900

    hashCode()のオーバーライド

--- src/Hero.java
+++ src/Hero.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Hero {
   private String name;
   private int hp, mp;
@@ -11,5 +13,17 @@ public class Hero {
   public String toString() {
     return "勇者(名前=" + this.name + "/HP="
         + this.hp + "/MP=" + this.mp + ")";
+  }
+
+  public boolean equals(Object o) {
+    if (o == this) return true;
+    if (o == null || !(o instanceof Hero)) return false;
+    Hero h = (Hero)o;
+    if (!this.name.trim().equals(h.name.trim())) return false;
+    return true;
+  }
+
+  public int hashCode() {
+    return Objects.hash(this.name, this.hp, this.mp);
   }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class Main {
   public static void main(String[] args) throws Exception {
-    Hero a = new Hero("Takeshi", 100, 100);
-    System.out.println(a);
+    Hero h1 = new Hero("Takeshi", 100, 100);
+    System.out.println(h1);
 
     Account ac1 = new Account("1234567");
     Account ac2 = new Account("1234567");
@@ -13,5 +13,12 @@ public class Main {
     int[] arr2 = {1, 2, 3, 4, 5};
     System.out.println("誤った判定：" + arr1.equals(arr2));
     System.out.println("正しい判定：" + Arrays.equals(arr1, arr2));
+
+    Set<Hero> list = new HashSet<>();
+    list.add(h1);
+    System.out.println("要素数=" + list.size());
+    h1 = new Hero("Takeshi", 100, 100);
+    list.remove(h1);
+    System.out.println("要素数=" + list.size());
   }
 }

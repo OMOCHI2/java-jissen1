740a80a31c4e0973d37e8258b3caf404dc130687
OMOCHI <satsukisama.mochi@gmail.com>
2023-10-21 23:28:52 +0900

    Arrays.equals(a, b)の活用

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Main {
   public static void main(String[] args) throws Exception {
     Hero a = new Hero("Takeshi", 100, 100);
@@ -6,5 +8,10 @@ public class Main {
     Account ac1 = new Account("1234567");
     Account ac2 = new Account("1234567");
     System.out.println(ac1.equals(ac2));
+
+    int[] arr1 = {1, 2, 3, 4, 5};
+    int[] arr2 = {1, 2, 3, 4, 5};
+    System.out.println("誤った判定：" + arr1.equals(arr2));
+    System.out.println("正しい判定：" + Arrays.equals(arr1, arr2));
   }
 }
